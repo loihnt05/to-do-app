1f7ecc94996d017a54e9c1f49112d10e7f495137
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-18 10:53:57 +0700

    react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import Layout from "@/components/Layout.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Layout/>}>
+                <Route index element={<App/>}></Route>
+            </Route>
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
