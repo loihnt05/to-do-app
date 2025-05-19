93daa7a9057b3179abf88181b085619c67bae370
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-19 21:17:46 +0700

    update final component todoitem

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -2,26 +2,35 @@ import { Checkbox } from '@/components/ui/checkbox.tsx';
 import { Label } from '@/components/ui/label.tsx';
 import { Button } from '@/components/ui/button.tsx';
 import * as React from 'react';
+import { Pencil, Trash } from 'lucide-react';
 
 function TodoItem() {
   const [checked, setChecked] = React.useState(false);
+  const [date] = React.useState(new Date());
+  const handleCheck = () => {
+    setChecked(!checked);
+  }
   return (
-    <div className={'flex items-center gap-2 w-full border p-3'}>
+    <div className={'flex items-center gap-2 w-full border p-2'}>
       {/*check*/}
-      <Checkbox id={'temp'} onCheckedChange={() => setChecked(!checked)} />
-      {/*content*/}
-      <Label htmlFor={'temp'}
-             className={'text-sm font-medium leading-none ' +
-               'peer-d' +
-               'isabled:cursor-not-allowed peer-disabled:opacity-70'
-               + `${(checked) ? ' line-through' : ''}`}>
-
-      </Label>
-
+      <Checkbox id={'temp'} onCheckedChange={handleCheck} className={'w-6 h-6 bg-gray-200 border'} />
+      <div className={'flex flex-col'}>
+        {/*content*/}
+        <Label htmlFor={'temp'}
+               className={'font-medium font-serif'
+                 + `${(checked) ? ' line-through decoration-1 opacity-50 decoration-gray-500 font-light italic' : ''}`}>
+          Hello my name is ho nguyen tai loi
+        </Label>
+        <p className={'font-sans text-xs font-light italic'}>{date.toLocaleString()}</p>
+      </div>
       {/*edit and remove*/}
       <div className={'grid grid-cols-2 ml-auto gap-1'}>
-        <Button className={''}>Edit</Button>
-        <Button>Delete</Button>
+        <Button className={'hover:cursor-pointer'}>
+          <Pencil></Pencil>
+        </Button>
+        <Button className={'hover:cursor-pointer'}>
+          <Trash></Trash>
+        </Button>
       </div>
     </div>
   );
