03537123b0a9d0a152bf2cb0d991940a8fb40a70
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-19 20:37:32 +0700

    prettier for reformat code

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -1,29 +1,30 @@
-import {Checkbox} from "@/components/ui/checkbox.tsx";
-import {Label} from "@/components/ui/label.tsx";
-import {Button} from "@/components/ui/button.tsx";
-import * as React from "react";
+import { Checkbox } from '@/components/ui/checkbox.tsx';
+import { Label } from '@/components/ui/label.tsx';
+import { Button } from '@/components/ui/button.tsx';
+import * as React from 'react';
 
 function TodoItem() {
-    const [checked, setChecked] = React.useState(false);
-    return (
-        <div className={'flex items-center gap-2 w-full border p-3'}>
-            {/*check*/}
-            <Checkbox id={'temp'}  onCheckedChange={() => setChecked(!checked)}/>
-            {/*content*/}
-            <Label htmlFor={'temp'}
-                   className={"text-sm font-medium leading-none " +
-                       "peer-d" +
-                       "isabled:cursor-not-allowed peer-disabled:opacity-70"
-                        + `${(checked) ? ' line-through' : ''}`}>
-                temp
-            </Label>
-            {/*edit and remove*/}
-            <div className={'grid grid-cols-2 ml-auto gap-1'}>
-                <Button className={''}>Edit</Button>
-                <Button>Delete</Button>
-            </div>
-        </div>
-    );
+  const [checked, setChecked] = React.useState(false);
+  return (
+    <div className={'flex items-center gap-2 w-full border p-3'}>
+      {/*check*/}
+      <Checkbox id={'temp'} onCheckedChange={() => setChecked(!checked)} />
+      {/*content*/}
+      <Label htmlFor={'temp'}
+             className={'text-sm font-medium leading-none ' +
+               'peer-d' +
+               'isabled:cursor-not-allowed peer-disabled:opacity-70'
+               + `${(checked) ? ' line-through' : ''}`}>
+
+      </Label>
+
+      {/*edit and remove*/}
+      <div className={'grid grid-cols-2 ml-auto gap-1'}>
+        <Button className={''}>Edit</Button>
+        <Button>Delete</Button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoItem;
