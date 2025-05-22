4a8b65d0629630451376200a632606a604ad33b0
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-22 10:07:55 +0700

    refractor new page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './styles/global.css'
 import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from "react-router";
 import Layout from "@/components/Layout.tsx";
+import TodoMain from '@/pages/todo-main.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
             <Route path='/' element={<Layout/>}>
                 <Route index element={<App/>}/>
+                <Route path={"/todo"} element={<TodoMain/>}/>
                 <Route path={"/test"} element={<div/>}/>
             </Route>
         </Routes>
